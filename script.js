49cbb3c2ce6387d786bd8800ff83437b13d15db9
Egor <hudyy@mail.ru>
2020-03-29 20:46:51 +0500

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,12 @@
+/* let persone = {
+    name: "john"
+};
+console.log(persone["name"]); */
+/* let arr = ['plum.png','orange.png'];
+
+console.log(arr[0]); */
+
+/* let answer = confirm("are yo old?");
+console.log(answer); */
+let answer = prompt("ssssss?", "ye")
+console.log(typeof(answer));
